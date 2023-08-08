c7c8e9d27557aaf50c487c042a35a8534042a745
zahradnik-ondrej <ondraz@pm.me>
2023-08-08 18:55:38 +0200

    waitForSelector hotfix

--- dist/src/functions/waitForSelector.js
+++ dist/src/functions/waitForSelector.js
@@ -2,20 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.waitForSelector = void 0;
 const SelectorNotFound_1 = require("../errors/SelectorNotFound");
+const TIMEOUT_STEP_1 = require("../constants/TIMEOUT_STEP");
 async function waitForSelector(page, selector, timeout = 60) {
     timeout *= 1000;
-    const TIMEOUT_STEP = 500;
     while (timeout > 0) {
         try {
-            await page.waitForSelector(selector, { timeout: TIMEOUT_STEP });
+            await page.waitForSelector(selector, { timeout: TIMEOUT_STEP_1.TIMEOUT_STEP });
+            break;
         }
         catch (e) {
-            timeout -= TIMEOUT_STEP;
-            if (timeout <= 0) {
-                throw new SelectorNotFound_1.SelectorNotFound(`Couldn't find selector: ${selector}`);
-            }
+            // this stays empty
         }
-        break;
+        timeout -= TIMEOUT_STEP_1.TIMEOUT_STEP;
+        if (timeout <= 0) {
+            throw new SelectorNotFound_1.SelectorNotFound(`Couldn't find selector: ${selector}`);
+        }
     }
 }
 exports.waitForSelector = waitForSelector;
