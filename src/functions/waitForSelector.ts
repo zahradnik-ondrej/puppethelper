a4c9f9619b589c74d63a89c2cfb5914285ba41d8
zahradnik-ondrej <ondraz@pm.me>
2023-08-08 18:06:38 +0200

    waitForSelector function update

--- src/functions/waitForSelector.ts
+++ src/functions/waitForSelector.ts
@@ -1,11 +1,26 @@
 import {Page} from 'puppeteer'
 import {SelectorNotFound} from '../errors/SelectorNotFound'
 
-async function waitForSelector(page: Page, selector: string): Promise<void> {
+async function waitForSelector(page: Page, selector: string, timeout: number = 5): Promise<void> {
+  /*
   try {
     await page.waitForSelector(selector)
   } catch(e) {
     throw new SelectorNotFound(`Couldn't find selector: ${selector}`)
+  }
+  */
+
+  timeout *= 1000
+  const TIMEOUT_STEP: number = 500
+  while(timeout > 0) {
+    try {
+      await page.waitForSelector(selector, {timeout: TIMEOUT_STEP})
+    } catch(e) {
+      timeout -= TIMEOUT_STEP
+      if (timeout <= 0) {
+        throw new SelectorNotFound(`Couldn't find selector: ${selector}`)
+      }
+    }
   }
 }
 
