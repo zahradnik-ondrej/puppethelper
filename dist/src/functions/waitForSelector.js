c95b282d65219d0b0be06b087aed219ff2f8d23c
zahradnik-ondrej <ondraz@pm.me>
2023-08-08 19:23:40 +0200

    waitForSelector hotfix...again

--- dist/src/functions/waitForSelector.js
+++ dist/src/functions/waitForSelector.js
@@ -3,11 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.waitForSelector = void 0;
 const SelectorNotFound_1 = require("../errors/SelectorNotFound");
 async function waitForSelector(page, selector, timeout = 60) {
-    try {
-        await page.waitForSelector(selector, { timeout: timeout * 1000 });
-    }
-    catch (e) {
-        throw new SelectorNotFound_1.SelectorNotFound(`Couldn't find selector: ${selector}`);
+    timeout *= 1000;
+    const TIMEOUT_STEP = 500;
+    while (timeout > 0) {
+        try {
+            await page.waitForSelector(selector, { timeout: TIMEOUT_STEP });
+            break;
+        }
+        catch (e) {
+            timeout -= TIMEOUT_STEP;
+            if (timeout <= 0) {
+                throw new SelectorNotFound_1.SelectorNotFound(`Couldn't find selector: ${selector}`);
+            }
+        }
     }
 }
 exports.waitForSelector = waitForSelector;
--- src/functions/waitForSelector.ts
+++ src/functions/waitForSelector.ts
@@ -2,10 +2,19 @@ import {Page} from 'puppeteer'
 import {SelectorNotFound} from '../errors/SelectorNotFound'
 
 async function waitForSelector(page: Page, selector: string, timeout: number = 60): Promise<void> {
-  try {
-    await page.waitForSelector(selector, { timeout: timeout * 1000 });
-  } catch (e) {
-    throw new SelectorNotFound(`Couldn't find selector: ${selector}`);
+  timeout *= 1000
+  const TIMEOUT_STEP: number = 500
+
+  while (timeout > 0) {
+    try {
+      await page.waitForSelector(selector, { timeout: TIMEOUT_STEP })
+      break
+    } catch (e) {
+      timeout -= TIMEOUT_STEP
+      if (timeout <= 0) {
+        throw new SelectorNotFound(`Couldn't find selector: ${selector}`)
+      }
+    }
   }
 }
 
