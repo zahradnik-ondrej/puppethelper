8e8e02f4254eb0ed837aefcc2714d98ab643bfbc
zahradnik-ondrej <ondraz@pm.me>
2023-08-08 19:36:41 +0200

    getText hotfix

--- dist/src/functions/getText.js
+++ dist/src/functions/getText.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getText = void 0;
-const waitForSelector_1 = require("./waitForSelector");
 async function getText(page, selector) {
-    await (0, waitForSelector_1.waitForSelector)(page, selector);
     return page.$eval(selector, (element) => (element.textContent || '').trim());
 }
 exports.getText = getText;
