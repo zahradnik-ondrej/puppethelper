afd16291ea542aa65f8c7010828a94e075a55a3e
zahradnik-ondrej <ondraz@pm.me>
2023-08-08 19:36:26 +0200

    getText hotfix

--- src/functions/getText.ts
+++ src/functions/getText.ts
@@ -1,8 +1,6 @@
 import {Page} from 'puppeteer'
-import {waitForSelector} from "./waitForSelector"
 
 async function getText(page: Page, selector: string): Promise<string> {
-  await waitForSelector(page, selector)
   return page.$eval(selector, (element: Element) => (element.textContent || '').trim())
 }
 
