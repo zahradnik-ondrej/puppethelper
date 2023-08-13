aa386b491d2335f82523788c6e448518171dc374
zahradnik-ondrej <ondraz@pm.me>
2023-08-13 20:52:05 +0200

    execSudoCommand fix

--- src/functions/execSudoCommand.ts
+++ src/functions/execSudoCommand.ts
@@ -1,14 +1,17 @@
 import {exec, ExecException} from 'child_process'
 
-async function execSudoCommand(command: string, password: string): Promise<void> {
-    exec(`echo ${password} | sudo -S ${command}`, { maxBuffer: 1024 * 10000 }, (error: ExecException | null, stdout: string, stderr: string): void => {
-        if (error) {
-            console.error(error)
-        }
-        if (stderr) {
-            console.warn(stderr)
-        }
-        console.log(stdout)
+function execSudoCommand(command: string, password: string): Promise<string> {
+    return new Promise((resolve, reject): void => {
+        exec(`echo ${password} | sudo -S ${command}`, { maxBuffer: 1024 * 10000 }, (error: ExecException | null, stdout: string, stderr: string): void => {
+            if (error) {
+                reject(error)
+                return
+            }
+            if (stderr) {
+                console.warn(stderr)
+            }
+            resolve(stdout)
+        })
     })
 }
 
