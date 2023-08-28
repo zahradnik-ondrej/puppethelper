36d3951a97ac453a236e420fea46b97d1ecbf1f8
zahradnik-ondrej <ondraz@pm.me>
2023-08-28 13:27:57 +0200

    added xpath click function

--- src/functions/click.ts
+++ src/functions/click.ts
@@ -1,9 +1,14 @@
-import {Page} from 'puppeteer'
-import {waitForSelector} from './waitForSelector'
+import { Page, ElementHandle } from 'puppeteer';
+import { waitForSelector } from './waitForSelector';
 
-async function click(page: Page, selector: string, timeout: number = 30): Promise<void> {
-    await waitForSelector(page, selector, timeout)
-    await page.click(selector)
+async function click(page: Page, selector: string, type: string = 'css', timeout: number = 30): Promise<void> {
+    if (type === 'css') {
+        await waitForSelector(page, selector, timeout);
+        await page.click(selector);
+    } else if (type === 'xpath') {
+        const [element] = await page.$x(selector);
+        await ((element as any) as ElementHandle).click();
+    }
 }
 
-export {click}
+export { click };
